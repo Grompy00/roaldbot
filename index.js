cc72a66ceda079b1a8b19d49d4f8d7c9c40212f3
Ben Kahlert <benjaminjosefkahlert@gmail.com>
2025-08-07 15:44:12 -0400

    Format file and consolidate messageCreate events

--- index.js
+++ index.js
@@ -1,52 +1,48 @@
-require('dotenv').config();
-const {Client, IntentsBitField} = require('discord.js')
+require("dotenv").config();
+const { Client, IntentsBitField } = require("discord.js");
+
 const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
 
-    intents:[
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent
-    ]
-})
+client.on("messageCreate", (message) => {
+  // Nothing if the message was from Roald
+  if (message.author.bot) {
+    return;
+  }
 
-//Respond with "Essentially"
-client.on('messageCreate', (message) => {
-    if(message.author.bot){
-        return
-    }
-    if (message.content.toLowerCase().includes("essentially")) {
-        message.reply('essentially <:percyshine:837514010404323449>')
-    }
-})
-//Respond with "Yeg"
-client.on('messageCreate', (message) => {
-    if(message.author.bot){
-        return
-    }
-    let randomNum = Math.floor(Math.random() * 1000)
-    if(message.content.trim().toLowerCase() === "yeg"){
-        if(randomNum % 10 === 2) {
-            message.reply('go fuck yourself')
-        return;
-    }}
-    if(message.content.includes("") && randomNum === 99){
-        message.reply('Yeg')
-    }
+  // Respond with "Essentially"
+  if (message.content.toLowerCase().includes("essentially")) {
+    message.reply("essentially <:percyshine:837514010404323449>");
+    return;
+  }
 
-})
+  let randomNum = Math.floor(Math.random() * 1000);
 
-client.on('messageCreate', (message) => {
-    if(message.author.bot){
-        return
+  // Respond with "Go fuck yourself."
+  if (message.content.trim().toLowerCase() === "yeg") {
+    if (randomNum % 10 === 2) {
+      message.reply("Go fuck yourself.");
+      return;
     }
-    let randomNum = Math.floor(Math.random() * 250)
-    if(message.content.includes("") && randomNum === 99){
-        message.reply('#KeepItKrunk')
-    }
+  }
 
-})
-client.login(process.env.TOKEN
-    
-);
+  // Respond with "Yeg"
+  if (message.content.includes("") && randomNum <= 1) {
+    message.reply("Yeg");
+    return;
+  }
 
+  // Respond with "#KeepItKrunk"
+  if (message.content.includes("") && randomNum > 1 && randomNum <= 5) {
+    message.reply("#KeepItKrunk");
+    return;
+  }
+});
+
+client.login(process.env.TOKEN);
