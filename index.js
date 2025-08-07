7c1d413a8325e2e639324394eebe8c636ad6d99a
Ben Kahlert <benjaminjosefkahlert@gmail.com>
2025-08-07 15:57:18 -0400

    Add yeg penalties

--- index.js
+++ index.js
@@ -10,14 +10,18 @@ const client = new Client({
   ],
 });
 
+const userYegMap = new Map();
+
 const oneInNChance = (n) => {
-    if (n <= 0) throw new Error("N must be a positive integer");
-    return Math.floor(Math.random() * n) === 0;
-  }
+  if (n <= 0) throw new Error("N must be a positive integer");
+  return Math.floor(Math.random() * n) === 0;
+};
 
 client.on("messageCreate", (message) => {
+  const user = message.author;
+
   // Nothing if the message was from Roald
-  if (message.author.bot) {
+  if (user.bot) {
     return;
   }
 
@@ -26,20 +30,39 @@ client.on("messageCreate", (message) => {
     message.reply("essentially <:percyshine:837514010404323449>");
   }
 
+  const existingYegsForUser = userYegMap.get(user.id);
+
   // Respond with "Go fuck yourself."
   if (message.content.trim().toLowerCase() === "yeg") {
-    if (oneInNChance(10)) {
+    if (typeof existingYegsForUser !== "undefined") {
+      // User has yegged before, increment their yeg count
+      existingYegsForUser++;
+      userYegMap.set(user.id, existingYegsForUser);
+    } else {
+      // User's first yeg, set them in the Map with a value of 1
+      existingYegsForUser = 1;
+      userYegMap.set(user.id, existingYegsForUser);
+    }
+
+    // For every yeg, the Go fuck yourself chance increases
+    const goFuckYourselfChance = Math.max(1, 10 - existingYegsForUser);
+    if (goFuckYourselfChance) {
       message.reply("Go fuck yourself.");
     }
   }
 
+  const yegPenalty = 20;
+  // Yeg response chance decreases for every time the user has yeg-d
+  const yegChance = 1000 + existingYegsForUser * yegPenalty;
+  const keepItKrunkChance = 250;
+
   // Respond with "Yeg"
-  if (message.content.includes("") && oneInNChance(1000)) {
+  if (message.content.includes("") && oneInNChance(yegChance)) {
     message.reply("Yeg");
   }
 
   // Respond with "#KeepItKrunk"
-  if (message.content.includes("") && oneInNChance(250)) {
+  if (message.content.includes("") && oneInNChance(keepItKrunkChance)) {
     message.reply("#KeepItKrunk");
   }
 });
